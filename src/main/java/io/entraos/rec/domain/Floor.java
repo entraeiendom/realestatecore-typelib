6129cd93d17cf1b84b1079f1eb5944f5d1eefaf9
baardl <bard.lind@gmail.com>
2020-04-28 16:21:03 +0200

    Initial setup

---
+++ src/main/java/io/entraos/rec/domain/Floor.java
@@ -0,0 +1,11 @@
+package io.entraos.rec.domain;
+
+public class Floor {
+    public static final String CLASS = "Storey";
+
+    private String uuid;
+    private String name;  //Eg 10
+
+
+
+}
---
+++ src/main/java/io/entraos/rec/domain/Room.java
@@ -0,0 +1,11 @@
+package io.entraos.rec.domain;
+
+public class Room {
+    public static final String CLASS = "Room";
+
+    private String uuid;
+    private String name;  //Eg 10104
+
+
+
+}
