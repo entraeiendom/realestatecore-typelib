b020bdb9831ebb2e89d886f8f8cc7c1c94eb3ac0
baardl <bard.lind@gmail.com>
2020-05-29 09:38:57 +0200

    Find RealEstate from search

--- src/test/java/io/entraos/rec/mappers/RealEstateListMapperTest.java
+++ src/test/java/io/entraos/rec/mappers/RealEstateListMapperTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.URI;
 import java.nio.file.Files;
 import java.util.List;
 
@@ -33,45 +32,5 @@ public class RealEstateListMapperTest {
         List<RealEstateCore> realEstates = RealEstateListMapper.fromJson(json);
         assertNotNull(realEstates);
         assertEquals(1, realEstates.size());
-    }
-
-    @Test
-    public void findNext() {
-        URI expected = URI.create("https://en.proptechos.com/api/realestatecomponent?page=1&size=5");
-        assertEquals(expected, BuildingComponentListMapper.findNext(json));
-    }
-
-    @Test
-    public void findFirst() {
-        URI expected = URI.create("https://en.proptechos.com/api/realestatecomponent?page=0&size=5");
-        assertEquals(expected, BuildingComponentListMapper.findFirst(json));
-    }
-
-    @Test
-    public void findLast() {
-        URI expected = URI.create("https://en.proptechos.com/api/realestatecomponent?page=160&size=5");
-        assertEquals(expected, BuildingComponentListMapper.findLast(json));
-    }
-
-    @Test
-    public void findCount() {
-        Number count = BuildingComponentListMapper.findCount(json);
-        assertEquals(count, BuildingComponentListMapper.findCount(json));
-    }
-
-    @Test
-    public void findFromProptechOs() throws Exception {
-        ClassLoader classLoader = new DeviceJsonMapperTest().getClass().getClassLoader();
-        File file = new File(classLoader.getResource("proptechos-buildingcomponents-find.json").getFile());
-        assertTrue(file.exists());
-
-        //Read File Content
-        json = new String(Files.readAllBytes(file.toPath()));
-        assertNotNull(json);
-        assertFalse(json.isEmpty());
-        List<RealEstateCore> buildingComponents = BuildingComponentListMapper.fromJson(json);
-        assertNotNull(buildingComponents);
-        assertEquals(2, buildingComponents.size());
-
     }
 }
